a9e4445715f61e54935489c6c7f94f930264d80b
Meleah Roe <melroe19@gmail.com>
2024-06-12 12:21:10 -0500

    added entree to favorite foods array

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -39,6 +39,8 @@ console.log('First animal is', firstAnimal);
 //      of the second item from the `animalArray`.
 //      Be sure to console.log this new variable to make sure your code
 //      worked as expected.
+let secondAnimal = animalArray[1];
+console.log('second animal is', secondAnimal);
 
 
 // 3.b. TODO: Create a variable `lastAnimal` and assign it the value of
@@ -46,9 +48,13 @@ console.log('First animal is', firstAnimal);
 //      You'll need to console.log `animalArray` and `lastAnimal` to make
 //      sure that your code does what you want. (Never trust your code until
 //      you have proof that it works!)
+let lastAnimal = animalArray[3];
+console.log('last animal in the animal array should be dog', lastAnimal);
 
 // 3.c. (STRETCH) TODO: Refactor 3.b to use the `animalArray` variable's .length 
 //       property, rather than the exact (hardcoded) index number of the last item.
+indexOfLastAnimal = animalArray.length -1;
+console.log('the index of the last animal is', indexOfLastAnimal );
 
 
 
@@ -64,6 +70,9 @@ console.log('Added an animal to end,', animalArray);
 //      a dessert that you love.
 //      Similar to above, add the `dessert` to the end of your `favoriteFoods`
 //      array. (How can you be 100% certain this worked? 🤔)
+let dessert = "strawberry pie";
+favoriteFoods.push(dessert);
+console.log('added a new dessert to end of favoriteFoods', favoriteFoods);
 
 
 // Example: Add to the beginning of an array using the Array.unshift method.
@@ -74,6 +83,9 @@ console.log(`Added an animal to beginning: ${animalArray}`);
 // 4.b. TODO: Create a variable `entree` and assign it a string value of
 //      an entree that you enjoy.
 //      Add the `entree` to the beginning of your `favoriteFoods` array.
+let entree = "lasagna";
+favoriteFoods.unshift(entree);
+console.log('added an entree to the beginning of favoritefoods array', favoriteFoods, entree);
 
 
 // Example: Remove the last animal by using the Array.pop method.
