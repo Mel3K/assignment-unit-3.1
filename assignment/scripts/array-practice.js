3ef877385667a6086587b1879ae17e3c68883c1d
Meleah Roe <melroe19@gmail.com>
2024-06-12 12:33:26 -0500

    removed first color in colors array and saved into a variable first color

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -102,6 +102,9 @@ let colors = ['orange', 'yellow', 'green', 'teal'];
 //      Don't forget! Use console.log to verify that the `colors`
 //      array only contains three items now, AND to verify that `lastColor`
 //      contains the value "teal".)
+let lastColor = colors.pop();
+console.log('last color in color array', lastColor);
+console.log('colors array is now', colors);
 
 
 // Example: Remove the first animal using the Array.shift method.
@@ -112,6 +115,8 @@ console.log('The animals are now', animalArray);
 // 4.d. TODO: Remove the first value from the `colors` array and store it
 //      inside a new variable called `firstColor`.
 //      To verify that your code worked, console.log `firstColor`!  
+let firstColor = colors.shift();
+console.log('colors array is now', colors, "first color is ", firstColor);
 
 
 
